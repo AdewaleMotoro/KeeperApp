319c1813ff938ca915d5803d47b666b030e1df75
AdeMotoro <93820103+AdewaleMotoro@users.noreply.github.com>
2023-08-16 13:52:37 +0100

    Initial commit

---
+++ KeeperApp/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../KeeperApp/Header";
+import Footer from "./Footer";
+import Note from "../KeeperApp/Note";
+import "../KeeperApp/styles.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
